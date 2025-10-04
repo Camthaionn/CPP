8ce1c6debae2bc38bcfe4d8b462475071b8f1e97
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-04 09:38:43 +0300

    Add files via upload

---
+++ 2.Variables.cpp
@@ -0,0 +1,22 @@
+﻿
+
+
+#include <iostream>
+
+int main()
+{
+	int var5, var6;   //This is an another way to store variables wthout values so that you can assign values later in code.
+	var5 = 17;
+	var6 = 23;
+	std::cout << var5 + var6;
+
+
+	int var1  = 9;   //This is a variable and it keeps vlues in it so that you can use them whenever you want.
+	int var2 = 10;
+	int res = var1 + var2;  //This is a new variable created by summorazing two variables
+	std::cout << res;   //By using this code you can see what is the value of this variable
+
+	int var7, Var7;   //These are two different variables remember that!
+
+}
+
