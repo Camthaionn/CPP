2b586143c11d1a421303e55f07eebfd0604e4c29
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-04 12:08:22 +0300

    Add files via upload

---
+++ 4.Basic Data Types 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <windows.h>
+#include <cwchar>
+int main()
+{
+  /*   bool a = true;
+        std::cout << sizeof(a);
+        //shows you the size of a boolean value
+   */
+
+    /*
+    auto d = 12.5;
+    std::cout << d;
+    //cpp understans automaticly tyoe of data but it spends time
+    */
+
+
+
+    
+}
+
