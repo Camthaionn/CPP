f5c4db847daf82f014e809f7f77d08ae3fa87197
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-05 15:40:10 +0300

    Add files via upload

---
+++ 7.Pointers.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    //int a = 10; 
+    //int* Aptr = &a; //This keeps the adress of the a value in the memory
+    //std::cout << &a <<'\n';  //adress of a variable
+    //std::cout << a << '\n';  //value of a variable
+    //std::cout << Aptr << '\n'; //By using this variable we can see the adress od a variable
+    //std::cout << *Aptr << '\n';  //This code gives you directly the value
+
+
+
+    //int b = 20;
+    //int* Bptr = &b;
+    //int * Bptr = &b;
+    //int  *Bptr = &b;
+    //int*Bptr = &b;
+    ////These all for same
+
+    //int a = 25;
+    //std::cout << &a <<'\n';   //This one gives the adress  --->000000059D9AFC24
+    //std::cout << *&a <<'\n';  //This one gives the value   --->25
+    //std::cout << &*&a << '\n' ; // --> 000000B2DBF6FC74
+    //std::cout << *&*&a << '\n' ; // --> 25
+    
+
+
+}
