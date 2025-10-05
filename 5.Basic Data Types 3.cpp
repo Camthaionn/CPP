6d9fce3bc1f0409ddd446fd027222aafa7df3d2d
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-05 14:30:27 +0300

    Add files via upload

---
+++ 5.Basic Data Types 3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <windows.h>
+#include <cwchar>
+
+int main()
+{
+    ////Literal Constants
+    //bool isActive{ true };
+    //std::cout << "Hello wolrd";
+
+
+    ////(Symbolic Constants)
+    //const int x{ 4 };
+    //x = 15;
+    ////This might be useful when we are working with for example pi = 3,14
+
+//
+//    //closed type conversion
+//    int a = 10;
+//    a = 1.4;
+//    std::cout << a;
+//    //İf you use variables like this you may lose the float value
+//}
+
+
+//// explict type conversion
+//    int a = 10;
+//    a = static_cast<int>(5.9);
+//    std::cout << a;
+
+
+}
