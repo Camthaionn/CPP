78b74674fd4b0fac0ce19830eab8895ab463f758
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 14:36:39 +0300

    Add files via upload

---
+++ 10.Functions3.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <string>
+#include <iostream>
+
+
+void print(std::string text) {
+    std::cout << text;
+}
+
+//For the case that user haven't given any messages
+void printV2(std::string text="default message") {
+    std::cout << text;
+}
+
+//Multiple parameter function
+void printV3(std::string text = "default message",int a = 20,bool as = false) {
+    std::cout << text << a << as;
+}
+
+int main()
+{
+    printV3("Hi",2,true);
+}
+
