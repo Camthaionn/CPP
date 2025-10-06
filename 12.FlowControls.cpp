9a3b023bde5091350b9235de57c9fd7ff193bcf5
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 15:52:40 +0300

    Add files via upload

---
+++ 12.FlowControls.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+
+
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//
+//    std::cin >> a;//Takes input from user
+//    std::cin >> b;
+//
+//    std::cout << a + b;
+//
+//}
+
+
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    std::cout << "Frist number" << std::endl;
+//    std::cin >> a;//Takes input from user
+//    std::cout << "Second number" << std::endl;
+//    std::cin >> b;
+//
+//    std::cout <<"Sonuc:"<< a + b;
+//
+//}
+
+
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    char op = '+';
+
+    std::cout << "Frist number" << std::endl;
+    std::cin >> a;//Takes input from user
+
+    std::cout << "Second number" << std::endl;
+    std::cin >> b;
+
+    std::cout << "Type your operator" << std::endl;
+    std::cin >> op; 
+
+    if (op == '+') {
+        std::cout << "Sonuc:" << a + b;
+    }
+    else {
+        std::cout << "Fail";
+    }
+
+}
+
+
+
+
