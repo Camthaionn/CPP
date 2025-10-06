b7a7a52f5c0822f616a2688ed1f4f28e3ee42983
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 17:40:56 +0300

    Add files via upload

---
+++ 15.FlowControls(For).cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()//main function to execute codes
+{
+    int length = 10; //Length of our for loop
+
+        for (int i = 0; i < length; i++) {//Our for loop
+            std::cout << "For loop" << i << std::endl;
+        }
+        std::cout << "Loop has ended";//lets us know that loop ended
+}
