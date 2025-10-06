a87587376aa9047a3082c3dc003ca9c1c2770cb1
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 16:53:08 +0300

    Add files via upload

---
+++ 13.FlowControls2.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+
+int main()//main function to execute codes
+{
+    int a = 0;//Variable
+    int b = 0;//Variable
+    
+
+    std::cout << "Frist number" << std::endl;
+    std::cin >> a;//Takes first input from user
+
+    std::cout << "Second number" << std::endl;
+    std::cin >> b;//Takes second input from user
+
+    
+
+    if (a < b) {//if statement to control condition
+        std::cout << "firs one is smaller" ;
+    }
+    else if(a == b) {
+        std::cout<<"Numbers are equal";
+    }
+    else {
+        std::cout << "second one is bigger";
+    }
+
+}
