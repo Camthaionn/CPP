e95d25013f872a1a1e5f0c891201b17a1f46d1dd
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 14:22:27 +0300

    Add files via upload

---
+++ 9.Functions2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+int sum(int var1, int var2) {
+	return var1 + var2;
+}
+
+int sum(double var1, double var2) {
+	return var1 + var2;
+}
+
+//Compiler can undesrtand which function it will be used byy the parameters ggiven. Name of function can same
+int main()
+{
+	int a = 5;
+	int b = 10;
+	double c = 10.5;
+	double d = 20.5;
+
+	std::cout << sum(a, b) << '\n';
+	std::cout << sum(c, d);
+
+}
+
