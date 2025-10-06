1ba5d278544c979ac0b46704a9f11b6c7d3dfae0
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-06 15:19:28 +0300

    Add files via upload

---
+++ 11.Functions4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+//
+//int sum(int a, int b) {
+//    return a + b;
+//}
+//
+//int bring(int (*fint)(int, int)) {
+//    int a = fint(10, 10);
+//    return a;
+//}
+//
+//int main() {
+//    int b = bring(sum);
+//    std::cout << b;
+//
+//
+//    std::cout << b;
+//}
+
+//---------------------------
+
+inline int topla(int a, int b) {
+	return a + b;
+}
+int main() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
