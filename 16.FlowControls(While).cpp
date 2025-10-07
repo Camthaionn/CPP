2bc94ea715d3ff60ecd0b0e2b26227e93d198cad
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-07 14:35:46 +0300

    Add files via upload

---
+++ 16.FlowControls(While).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+int main()//main function to execute codes
+{
+    int a = 0;//Variable
+    int b = 0;//Variable
+    char op = 'a';
+
+    
+    while (op != 'q') {//Ends the loop if input from user is "q"
+        std::cout << "Frist number" << std::endl;
+        std::cin >> a;//Takes first input from user
+
+        std::cout << "Second number" << std::endl;
+        std::cin >> b;//Takes second input from user
+
+        std::cout << "Choose your operator (+, -, *, /) or press q to quit ";
+        std::cin >> op;//Takes the oprerator which user given
+
+        if (op == 'q')//If input is q quit directly from while loop
+            break;
+
+
+        switch (op) {//This swtich helps you to control the flow 
+        case '+'://By this case you can sum your variable and end the program
+            std::cout << "Result:" << a + b << std::endl;
+            break;
+        case '-'://By this case you can substract your variable and end the program
+            std::cout << "Result:" << a - b << std::endl;
+            break;
+        case '*'://By this case you can multiply your variable and end the program
+            std::cout << "Result:" << a * b << std::endl;
+            break;
+        case '/'://By this case you can divide your variable and end the program
+            std::cout << "Result:" << a / b << std::endl;
+            break;
+        default://İf program can'not go inside of the cases it goes inside of default
+            std::cout << "Unknown process!" << std::endl;
+            break;
+        }
+
+        
+
+    }
+    std::cout << "Program has ended" << std::endl;//To inform user that the program has ended
+}
