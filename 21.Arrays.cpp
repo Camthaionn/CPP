3d823875d65f5a538d59449f06f0e853617c4375
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-07 19:20:19 +0300

    Add files via upload

---
+++ 21.Arrays.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void test(int arr[3], int size) {
+
+
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+    }
+}
+
+int main() {
+    int arr[3] = { 1, 2, 3 };
+    int size = 3;
+
+    test(arr, size);
+}
