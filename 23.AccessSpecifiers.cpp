56201efc554fd4eb0edfd2d809fed3dff3ec4e45
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-09 15:45:04 +0300

    Add files via upload

---
+++ 23.AccessSpecifiers.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+//class Payment {
+//private:
+//    std::string hasCode = "123456789";  //We dont want this yo be changed so its under de private.
+//
+//public:
+//    std::string moneyType = "usd";
+//    std::string cost = "0.0";
+//   
+//
+//    void pay() {
+//        std::cout << "We are sending your data to Bank. Money type: " + this->moneyType +
+//            " Cost:" + this->cost + " Code:" + this->hasCode;
+//    }
+//};
+
+
+class Payment {
+protected:
+    std::string hasCode = "123456789";  //We dont want this yo be changed so its under de private.
+
+public:
+    std::string moneyType = "usd";
+    std::string cost = "0.0";
+
+
+    void pay() {
+        std::cout << "We are sending your data to Bank. Money type: " + this->moneyType +
+            " Cost:" + this->cost + " Code:" + this->hasCode;
+    }
+};
+
+//Because that the payment class is protected we can use it in this class.Its inheritance.
+class Test :public Payment {
+public:
+    void hello() {
+        Test::hasCode = "asdasd";
+    }
+};
+
+int main() {
+    Payment payment;
+    payment.moneyType = "usd";
+    payment.cost = "50.6";
+    payment.pay();
+    return 0;
+}
