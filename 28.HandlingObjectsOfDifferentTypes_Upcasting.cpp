ab8c8decfe72e023f95ce0bcc60c3475401981c8
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-11 16:18:12 +0300

    Add files via upload

---
+++ 28.HandlingObjectsOfDifferentTypes_Upcasting.cpp
@@ -0,0 +1,57 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class Weapon {
+public:
+	string name;
+	int magazineCapacity;
+	string color;
+
+};
+
+class Ak47 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	bool scope;
+
+	void shoot() {
+
+	}
+};
+
+class Pistol : public Weapon {
+public:
+	int chargingHandlePattern;
+
+	void shoot() {
+
+	}
+};
+
+class M1 :public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	int chragingHandlePattern;
+
+	void shoot() {
+
+	}
+};
+
+int main() {
+
+	Ak47 ak47;
+	ak47.name = "test"; 
+
+	Weapon* weapPtr = &ak47; //Giving the ak47 object like a weapon not ak47
+
+	weapPtr->name = "notest";  // So we can change the name
+	cout << ak47.name;
+				//we can change the qualifications of the objeect as it is a weapon not Ak47 --up cast
+
+	return 0;
+}
