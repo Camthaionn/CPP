a746dfd433603b37717f754c5f1c5cfb5c33b39d
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-13 17:49:02 +0300

    Add files via upload

---
+++ 31.Encapsulation.cpp
@@ -0,0 +1,36 @@
+﻿
+//Encapsulation exapmle
+#include <iostream> 
+using namespace std;
+
+class Payment {
+private:
+	float cost;
+public:
+	string moneyType;
+
+	void setCost(float c) {
+		if (c > 0) {
+			cost = c;
+		}
+		else {
+			cost = 0.1;
+		}
+	}
+
+	void pay() {
+		cout<< cost << ":Payment has been taken";
+	}
+};
+
+
+
+int main() {
+	Payment pay;
+	pay.setCost(-34.4);
+	pay.moneyType = "usd";
+	pay.pay();
+
+
+	return 0;
+};
