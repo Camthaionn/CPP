9e2f6cebf41e1f1fdde24dc387551d868606d14f
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-13 17:34:41 +0300

    Add files via upload

---
+++ 30.CastingExample.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+class Weapon {
+public:
+	string name;
+	int magazineCapacity;
+	string color;
+
+};
+
+class Ak47 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	bool scope;
+
+	void shoot() {
+
+	}
+};
+
+class Pistol : public Weapon {
+public:
+	int chargingHandlePattern;
+
+	void shoot() {
+
+	}
+};
+
+class M1 :public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	int chragingHandlePattern;
+
+	void shoot() {
+
+	}
+};
+
+void Show(Weapon* WeaponPtr) {
+	cout << WeaponPtr->name << endl;
+}
+
+int main() {
+
+	Ak47 ak47;
+	ak47.name = "Ak47";
+
+	Show(&ak47);
+
+
+	Pistol pistol;
+	pistol.name = "Pistol";
+
+	Show(&pistol);
+
+	M1 m1;
+	m1.name = "M1";
+
+	Show(&m1);
+
+
+
+	return 0;
+}
