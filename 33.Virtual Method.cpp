5a7f9be5ec90573e398c0f4be748558499274196
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-14 12:03:17 +0300

    Add files via upload

---
+++ 33.Virtual Method.cpp
@@ -0,0 +1,69 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+
+
+class Weapon {
+public:
+	string name;
+	int magazineCapacity;
+	string color;
+
+	virtual void shoot() = 0; //Virtual method so we can call gun noises
+
+};
+
+class Ak47 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	bool scope;
+
+	void shoot() {
+		cout << "bom bom";
+	};
+};
+
+class Pistol : public Weapon {
+public:
+	int windingHandlePattern;
+
+	void shoot() {
+
+		cout << "bam bam";
+
+	}
+
+};
+
+
+class M1 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	int windingHandlePosition;
+
+	void shoot() {
+		cout << "bum bum";
+	};
+};
+
+void shotShoot(Weapon* weaponPtr) {
+	weaponPtr->shoot();
+}
+
+
+int main() {
+
+	Ak47 ak47;
+	Pistol pistol;
+	M1 m1;
+
+	shotShoot(&ak47);
+	shotShoot(&pistol);
+	shotShoot(&m1);
+
+	return 0;
+};
