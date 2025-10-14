7eac078008157aaa465e1492dbbbc755f65d4a38
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-14 11:28:03 +0300

    Add files via upload

---
+++ 32.This Parameter.cpp
@@ -0,0 +1,55 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+class Weapon {
+public:
+	string name;
+	int magazineCapacity;
+	string color;
+
+};
+
+class Ak47 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	bool scope;
+
+	void shoot(string name) {
+		this->name = name;		//This method lets the compiler know its in this class
+	};
+};
+
+class Pistol : public Weapon {
+public:
+	int windingHandlePattern;
+
+	void shoot() {
+
+	}
+
+};
+
+
+class M1 : public Weapon {
+public:
+	bool burstSingle;
+	int slingLength;
+	int windingHandlePosition;
+
+	void shoot() {
+	};
+};
+
+
+int main() {
+
+	Ak47 ak47;
+	ak47.shoot("Semi automatic");
+
+
+
+	return 0;
+};
