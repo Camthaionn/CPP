31c179bf0981be4b8d66b493032139477f61a4f3
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-18 15:12:31 +0300

    Add files via upload

---
+++ 38.Abstraction3.cpp
@@ -0,0 +1,83 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+//This is an abstract class
+class Animal {  //You can not create an object from abstarct class they  manage classes
+    
+public:
+    int a;
+    virtual void noise() = 0; 
+};
+
+
+
+//You can define variables in abstract classes
+//Abstract classes include virtual methods.
+//Abstract classes functions dont have function operating space
+//They are similar to intrfaces
+
+
+
+//This is an interface.Interface doesnt include variable
+class Animal1 {
+public:
+   
+    virtual void noise() = 0;
+};
+
+
+
+
+
+class Dog : public Animal {
+public:
+    void noise() {
+        cout << " rough rough ";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void noise() {
+        cout << "meow meow ";
+    }
+};
+
+class Duck : public Animal {
+public:
+    void noise() {
+        cout << "quack quack ";
+    }
+};
+
+class Mause : public Animal {
+public:
+    void noise() {
+        cout << "sadfas  ";
+    }
+};
+
+void allNoises(Animal* animalPtr) {
+    animalPtr->noise();
+
+}
+
+
+
+int main()
+{
+    Dog dog1;
+    Cat cat1;
+    Duck duck1;
+    Mause mause1;
+    allNoises(&mause1);
+
+    
+}
+
