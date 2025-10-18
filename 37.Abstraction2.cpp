9d9f0e87b0db4651521419bc77c2e93808cec0f0
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-18 15:02:18 +0300

    Add files via upload

---
+++ 37.Abstraction2.cpp
@@ -0,0 +1,55 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class Animal {  //abstract class  now all heritenced classes have to execute noise()
+public:
+    virtual void noise() = 0;  //abstraction
+
+};
+
+class Dog : public Animal {
+public:
+    void noise() {
+        cout << " rough rough ";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void noise() {
+        cout << "meow meow ";
+    }
+};
+
+class Duck : public Animal {
+public:
+    void noise() {
+        cout << "quack quack ";
+    }
+};
+
+class Mause : public Animal {
+public:
+    void noise() {
+        cout << "sadfas  ";
+    }
+};
+
+void allNoises(Animal* animalPtr) {
+    animalPtr->noise();
+
+}
+
+
+
+int main()
+{
+    Dog dog1;
+    Cat cat1;
+    Duck duck1;
+    Mause mause1;
+    allNoises(&mause1);
+}
+
