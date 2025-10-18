10c544d95f4b8f7d62b28245edfa48ed2f38f3e9
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-18 15:36:50 +0300

    Add files via upload

---
+++ 40.Const.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream >
+
+// const object
+// const method
+// const variable
+// const parameter
+// const pointer
+// return const
+
+class Weapon {
+private:
+	int value = 20;
+public:
+	const int* get() {
+		
+		return &value;
+	}
+};
+
+
+
+
+int main() {
+	Weapon weapon;
+	*(weapon.get()) = 100;
+}// we cant change the value because its const
