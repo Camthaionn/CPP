32ad06b6d9ed6304db586bfe84fa76c48918665d
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-18 16:09:32 +0300

    Add files via upload

---
+++ 41.Const2.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream >
+using namespace std;
+
+// const object
+// const method
+// const variable
+// const parameter
+// const pointer
+// return const
+
+class Weapon {
+private:
+	int value = 20;
+public:
+
+	int* get(const Weapon* ptr) {
+		ptr->value = 50;
+		return &value;
+	}
+
+	int* get(Weapon* const ptr) {
+		Weapon s1;
+		ptr = &s1;
+		return &value;
+	}
+
+
+	int getAnother() {
+		return value;
+	}
+};
+
+
+
+int main() {
+	Weapon weapon;
+	weapon.get(&weapon);
+
+	cout << weapon.getAnother();
+}
