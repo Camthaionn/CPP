9b24dace6bcb4e4cc18ea3e5dee4e960137cf849
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-20 12:16:02 +0300

    Add files via upload

---
+++ 42.Static.cpp
@@ -0,0 +1,62 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+
+////Statick Memory 
+//void test() {
+//	static int a = 0;
+//	a++;
+//	cout << a << endl;
+//
+//}
+
+//class Book {
+//public:
+//	static int number;
+//
+//	Book() {
+//		number++;
+//	}
+//};
+//
+//int Book::number = 0;
+//
+//
+//int main() {
+//	Book book1;
+//	Book book2;
+//	Book book3;
+//	Book book4;
+//	
+//	cout << Book::number;
+//}
+
+class Book {
+public:
+	static int number;
+
+	Book() {
+		number++;
+	}
+
+	static int get() {
+		return number;
+	}
+};
+
+int Book::number = 0;
+
+void test() {
+	static Book book;
+	Book book2;
+	Book book3;
+}
+
+int main() {
+
+	test();
+
+	cout << Book::get();
+}
