2705c2ba8757700704d45dee834e7d9de31f706e
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-21 12:33:49 +0300

    Add files via upload

---
+++ 44.Exceptions.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double divide(int a, int b) {
+    
+    try {
+        if (b < 1) {
+            
+            throw - 1;  
+        }
+        return a / b;
+    }
+    catch (int) {
+        cout << "a number cannot divided by zero";
+    }
+
+}
+
+int main()
+{
+    cout << divide(10, 5);
+}
