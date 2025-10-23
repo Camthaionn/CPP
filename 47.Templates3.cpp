36583b106753ceb04734c57884a48c80c125a53a
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-23 16:34:04 +0300

    Add files via upload

---
+++ 47.Templates3.cpp
@@ -0,0 +1,69 @@
+﻿
+
+
+#include <iostream>
+
+//
+//template<typename T>
+//class Math {
+//public:
+//	T a{};
+//	T b{};
+//
+//	int sum() {
+//		return a + b;
+//	}
+//};
+//
+//
+//class Weapon {};
+//class Bullet {};
+//
+//
+//template<class T>
+//T*  createObject() {
+//	T nesne;
+//	return &nesne;
+//
+//}
+//
+//
+//
+//int main() {
+//	//We have sent class
+//	std::cout << createObject<Weapon>();
+//
+//}
+
+
+template<typename T>
+class Math {
+public:
+	T a{};
+	T b{};
+
+	int sum() {
+		return a + b;
+	}
+};
+
+
+class Weapon {};
+class Bullet {};
+
+
+template<typename T>
+T* createObject(T nesne) {
+	return &nesne;
+
+}
+
+
+
+int main() {
+
+	//now we have sent an object
+	Weapon weapon;
+	std::cout << createObject<Weapon>(weapon);
+
+}
