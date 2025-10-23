96c20c258c1f9beb72b23a9148b7c5c93139ee43
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-23 16:00:33 +0300

    Add files via upload

---
+++ 45.Templates.cpp
@@ -0,0 +1,23 @@
+﻿
+
+
+#include <iostream>
+
+//We use template method so we dont have to create another functions for different variables
+//because template does it for us
+template<typename T>
+
+T max(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+int main()
+{
+
+    //as you can see different type of variables are sent and
+    //no problem benefit of template method
+    std::cout << max(23, 430)<<std::endl;
+    std::cout << max(33.4, 35.4)<<std::endl;
+    
+}
+
