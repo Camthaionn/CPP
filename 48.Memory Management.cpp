8ba15038c07656b78d89af380922f94aa29b9e56
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-10-29 19:58:35 +0300

    Add files via upload

---
+++ 48.Memory Management.cpp
@@ -0,0 +1,30 @@
+﻿
+
+
+#include <iostream>
+
+class Weapon {
+public:
+    //Constructor method
+    Weapon() {
+        std::cout << "Created" << std::endl;
+    }
+    //Deconstructor method
+    ~Weapon() {
+        std::cout << "Deleted" << std::endl;
+    }
+};
+
+int main()
+{
+    //object created
+    Weapon* weapon;
+
+    //Heap memory
+    weapon = new Weapon;
+    
+    //deleted 
+    delete weapon;
+
+    std::cout << "Program has ended\n";
+}
