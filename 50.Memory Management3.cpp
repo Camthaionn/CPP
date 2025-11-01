e1d04e7b95671d5779d72e957cae8629c983df6d
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-01 18:40:08 +0300

    Add files via upload

---
+++ 50.Memory Management3.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+
+
+class Summarize {
+public:
+    //pointers are pointing a null place to not cause a problem
+    int* a = nullptr;
+    int* b = nullptr;
+
+    Summarize(int sumFirstNumber, int sumSecondNumber) {
+        //We prepare stroge for new variables in heap
+        a = new int;
+        b = new int;
+
+        //Parameters assigned to variables
+        *a = sumFirstNumber;
+        *b = sumSecondNumber;
+
+    }
+
+    ~Summarize() {
+        //WE have to delete the variables to aviod memory leak
+        delete a;
+        delete b;
+        std::cout << "Deleted";
+    }
+};
+
+
+
+int main()
+{
+    //Creating new object and sending parameters
+    Summarize* sum = new Summarize(10, 10);
+
+    //object has deleted
+    delete sum;
+}
