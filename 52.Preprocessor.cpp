7777e1bbc3f9cfc478c7da80acc0feda0981121e
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-01 19:28:24 +0300

    Add files via upload

---
+++ 52.Preprocessor.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+
+//macro varieble
+#define Number 1
+
+//macro function
+#define Summarize(a,b) (a+b)
+
+
+int main()
+{
+    //calling a macro function which have macro parameters
+    std::cout << Summarize(Number, Number);
+}
