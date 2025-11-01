226bc9d217ef5b1ef8b99f0de7f82fade43654ca
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-01 16:18:23 +0300

    Add files via upload

---
+++ 49.Memory Management2.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arraySize = 0;
+
+	cout << "What is the size of array? ";
+	cin >> arraySize;
+
+	//we are using heap so that we can decide size
+	//after we start the progmra otherwise we cant
+	int* array = new int[arraySize];
+
+	for (int i = 0; i < arraySize; i++) {
+		cout << "Please type the " << i+1 << ". elements of array ";
+		cin >> array[i];
+	}
+	
+	cout << "Elements of your array "<<endl;
+
+	for(int i = 0;i < arraySize;i++){
+		cout << array[i];
+	}
+
+}
+
