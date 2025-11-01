4fda491d04832fe57f5d384ca88192bb4b57789d
Hamza Çolak <150460474+Camthaionn@users.noreply.github.com>
2025-11-01 19:08:37 +0300

    Add files via upload

---
+++ 51.Memory Management4.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+
+class sharedPointer {
+public:
+    int d{};
+
+    sharedPointer() {
+        std::cout << "Created" << std::endl;
+    }
+
+    ~sharedPointer() {
+        std::cout << "Deleted" << std::endl;
+    }
+};
+
+
+int main()
+{
+    std::shared_ptr<sharedPointer> ptr = std::make_shared<sharedPointer>();
+
+    std::shared_ptr<sharedPointer> ptr2 = ptr;
+
+    //This functions counts how many pointers point the variable
+    std::cout << ptr.use_count()<<std::endl;
+}
